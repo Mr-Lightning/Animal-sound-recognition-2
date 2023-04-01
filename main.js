32c28f3346ac7643c09a0449eed458022937ccc1
Ayush <104489493+Mr-Lightning@users.noreply.github.com>
2023-04-01 19:00:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function startClassifictaion() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/XSipLvQmK/model.json',modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResult);
+}
